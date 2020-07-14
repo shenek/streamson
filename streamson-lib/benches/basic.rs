057cfbaf5b9b8a4cc2aea5f8866149348d9287c4
Stepan Henek <stepan@henek.name>
2020-07-14 09:22:05 +0200

    fix: make bencmark working properly

--- streamson-lib/benches/basic.rs
+++ streamson-lib/benches/basic.rs
@@ -15,14 +15,14 @@ pub fn simple(c: &mut Criterion) {
     let mut input = Vec::new();
     input.push(br#"{ "users": ["#.to_vec());
     for _ in 0..10_000 {
-        input.push(br#"{"a": "c"}"#.to_vec());
+        input.push(br#"{"a": "c"},"#.to_vec());
     }
-    input.push(br#""last"], {"logs":"#.to_vec());
+    input.push(br#""last"], "logs": ["#.to_vec());
     for _ in 0..50_000 {
-        input.push(br#"{"l": "ll"}"#.to_vec());
+        input.push(br#"{"l": "ll"},"#.to_vec());
     }
     input.push(br#""last"]}"""#.to_vec());
-    let input = vec![b"{".to_vec(), b"}".to_vec()];
+    input.push(br#"}"#.to_vec());
 
     let mut group = c.benchmark_group("simple");
     group.bench_function("simple buffer handler", |b| {
