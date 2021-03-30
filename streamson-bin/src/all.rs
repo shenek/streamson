23daeeb58e52e9100e522412cbf41403150eb3a0
Stepan Henek <stepan@henek.name>
2021-03-30 22:38:55 +0200

    feature(bin): all strategy is able to convert data (if right handler is used)

--- streamson-bin/src/all.rs
+++ streamson-bin/src/all.rs
@@ -6,7 +6,7 @@ use std::{
 
 use clap::{App, ArgMatches};
 use streamson_lib::{
-    handler,
+    handler::{self, Handler},
     strategy::{self, Strategy},
 };
 
@@ -26,18 +26,49 @@ pub fn process_all(matches: &ArgMatches, buffer_size: usize) -> Result<(), Box<d
         .map(|(_, handler)| Arc::new(Mutex::new(handler)))
         .collect();
 
+    let converter = hndlrs.iter().any(|e| e.lock().unwrap().is_converter());
+    if converter {
+        all.set_convert(converter);
+    }
+
     for handler in hndlrs.iter() {
         all.add_handler(handler.clone());
     }
 
     let mut buffer = vec![];
+    let mut output_conv = strategy::OutputConverter::new();
     while let Ok(size) = stdin().take(buffer_size as u64).read_to_end(&mut buffer) {
         if size == 0 {
             break;
         }
-        all.process(&buffer[..size])?;
-        stdout().write_all(&buffer[..size])?;
+
+        let output = all.process(&buffer[..size])?;
+        if converter {
+            let output: Vec<u8> = output_conv
+                .convert(&output)
+                .into_iter()
+                .map(|e| e.1)
+                .flatten()
+                .collect();
+            stdout().write_all(&output)?;
+        } else {
+            stdout().write_all(&buffer[..size])?;
+        }
+
         buffer.clear();
+    }
+
+    if converter {
+        // Input terminated try to hit strategy termination
+        let output = output_conv
+            .convert(&all.terminate()?)
+            .into_iter()
+            .map(|e| e.1)
+            .collect::<Vec<Vec<u8>>>();
+
+        for data in output {
+            stdout().write_all(&data)?;
+        }
     }
 
     for handler in hndlrs.clone() {
